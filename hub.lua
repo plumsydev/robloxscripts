0e0d35fa62c7ab31dd27a2bc3ce13bd2ed8a475d
plumsy@github:~# <contact@plumsy.dev>
2025-10-22 08:38:44 +0200

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -117,7 +117,7 @@ local supportedGames = {
         [1] = "Be an Egg",
         [2] = "BeAnEgg"
     },
-    [82647344334312] = {
+    [15885874861] = {
         [1] = "Be an Egg",
         [2] = "BeAnEgg"
     }
@@ -236,6 +236,7 @@ else
     end
 
 end
+
 
 
 
